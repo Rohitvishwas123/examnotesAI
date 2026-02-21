785d8d15b0a276a8c3ac4aa38b3b2766d71fc846
Rohitvishwas123 <chhabinath786@gmail.com>
2026-02-21 23:21:22 +0530

    connected frontend to live backend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,7 @@ import Notes from './pages/Notes'
 import Pricing from './pages/Pricing'
 import PaymentSuccess from './pages/PaymentSuccess'
 import PaymentFailed from './pages/PaymentFailed'
-export const serverUrl = "http://localhost:8000"
+const serverUrl = "https://examnotesai-j0sq.onrender.com"
 
 function App() {
   const dispatch = useDispatch()
